3762e4bb2e606c98c30673ea53eb3b873390689c
Aktan <you@example.com>
2024-02-02 12:30:28 +0300

    two

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,10 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+export default function greetUser() {
 console.log("Welcome to the Brain Games!");
+const name = readlineSync.question('May I have your name?');
+console.log(`Hello, ${name}!`);
+};
+
+greetUser();
